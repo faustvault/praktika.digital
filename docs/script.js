1aeceea89710b54219cc104c5246b07e02babae5
Philipp U <faustvault@users.noreply.github.com>
2021-09-20 14:52:42 +0300

    ticket practice update 25/09

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,14 @@
+const ticketElm = document.getElementById("ticket")
+const { x, y, width, height } = ticketElm.getBoundingClientRect()
+const centerPoint = { x: x + width / 2, y: y + height / 2 }
+
+window.addEventListener("mousemove", e => {
+  const degreeX = (e.clientY - centerPoint.y) * 0.008
+  const degreeY = (e.clientX - centerPoint.x) * -0.008
+  
+  ticketElm.style.transform = `perspective(1000px) rotateX(${degreeX}deg) rotateY(${degreeY}deg)`;
+})
+
 var stats = new Stats();
 stats.showPanel(0);
 document.body.appendChild(stats.dom);
